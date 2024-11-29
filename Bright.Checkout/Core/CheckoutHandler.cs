c3d731ecd4101632bdc3bfd2256a24e5f483f04a
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-29 08:46:27 +0000

    Implemented ProcessCheckout and linked to ICheckoutHandler interface

--- Bright.Checkout/Core/CheckoutHandler.cs
+++ Bright.Checkout/Core/CheckoutHandler.cs
@@ -1,11 +1,44 @@
+using Bright.Checkout.Contracts.Core;
 using Bright.Checkout.Contracts.Services;
+using Bright.Checkout.Extensions;
 
-namespace Bright.Checkout.Core
+namespace Bright.Checkout.Core;
+
+/// <summary>
+/// Represents a handler for the checkout process.
+/// </summary>
+/// <param name="checkoutService">The service responsible for checkout operations.</param>
+public class CheckoutHandler(ICheckoutService checkoutService) : ICheckoutHandler
 {
-    public class CheckoutHandler(ICheckoutService checkoutService)
+    /// <summary>
+    /// Processes the checkout by scanning items and calculating the total price.
+    /// </summary>
+    /// <remarks>
+    /// This method prompts the user to scan items until 'BUY' is entered.
+    /// It then calculates and displays the total cost of the scanned items.
+    /// </remarks>
+    public void ProcessCheckout()
     {
-        public bool ProcessCheckout()
+        string productCodeInput;
+        Console.WriteLine("### Checkout Initialised ###");
+        
+        do
         {
-        }
+            Console.Write("Scan an item to continue or type BUY to stop: ");
+            productCodeInput = ConsoleExtensions.GetCleanStringToUpper();
+
+            if (productCodeInput != "BUY")
+            {
+                bool scannedSuccessfully = checkoutService.Scan(productCodeInput);
+                
+                Console.WriteLine(scannedSuccessfully
+                    ? $"Product with {productCodeInput} scanned successfully."
+                    : $"Failed to scan product with SKU {productCodeInput}.");
+            }
+        } while (productCodeInput != "BUY");
+        
+        Console.WriteLine("### Checking Out ###");
+        int totalCost = checkoutService.CalculateTotalPrice();
+        Console.WriteLine($"### Your Total Came to: {totalCost} ###");
     }
 }
