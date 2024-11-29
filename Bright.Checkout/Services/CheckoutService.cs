e5bd261caee630ea8dcd034fe02a21a8a7f59d61
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-29 02:50:57 +0000

    Implemented GetTotalPrice and renamed namespace

--- Bright.Checkout/Services/CheckoutService.cs
+++ Bright.Checkout/Services/CheckoutService.cs
@@ -1,8 +1,9 @@
-﻿using Bright.Kata.Contracts.Data;
-using Bright.Kata.Contracts.Services;
-using Bright.Kata.Model;
+﻿using Bright.Checkout.Extensions;
+using Bright.Checkout.Contracts.Data;
+using Bright.Checkout.Contracts.Services;
+using Bright.Checkout.Model;
 
-namespace Bright.Kata.Services;
+namespace Bright.Checkout.Services;
 
 /// <summary>
 ///     Represents a service for managing checkout operations.
@@ -12,13 +13,13 @@ public class CheckoutService(IProductRepository productRepository) : ICheckoutSe
 {
     // Initialize dictionary for storing product information.
     private readonly Dictionary<string, Product> _products = productRepository.GetProducts();
-    
+
     // Initialize dictionary for storing scanned items and their quantities.
     private readonly Dictionary<string, BasketItem> _basket = new();
 
 
     /// <summary>
-    /// Scans an item by and adds it to the current checkout session.
+    ///     Scans an item by and adds it to the current checkout session.
     /// </summary>
     /// <param name="productCode">The unique code identifying the item to be scanned.</param>
     /// <exception cref="KeyNotFoundException">Thrown when the product with the specified item code is not found.</exception>
@@ -48,11 +49,18 @@ public class CheckoutService(IProductRepository productRepository) : ICheckoutSe
     }
 
     /// <summary>
-    ///     Calculates and returns the total price of all scanned items in the current checkout session.
+    ///     Calculates the total price of all items in the current checkout session.
     /// </summary>
-    /// <returns>The total price as an integer, representing the sum in the smallest currency unit (e.g., cents).</returns>
+    /// <returns>The total price of all items in the basket.</returns>
     public int GetTotalPrice()
     {
-        throw new NotImplementedException();
+        int totalPrice = 0;
+
+        foreach (BasketItem basketItem in _basket.Values)
+        {
+            totalPrice += basketItem.GetTotalPrice();
+        }
+
+        return totalPrice;
     }
 }
