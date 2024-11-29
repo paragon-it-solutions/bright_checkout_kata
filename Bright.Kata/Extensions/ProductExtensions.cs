49cbc777a2d39a0e4fe8f2e8d8d022e04ddedca0
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-29 01:46:47 +0000

    Created extension method for product to SetPricing

---
+++ Bright.Kata/Extensions/ProductExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Bright.Kata.Model;
+
+namespace Bright.Checkout.Extensions;
+
+/// <summary>
+/// Provides extension methods for the Product class.
+/// </summary>
+public static class ProductExtensions
+{
+    /// <summary>
+    /// Sets the pricing rule for a product.
+    /// </summary>
+    /// <param name="product">The product to update.</param>
+    /// <param name="pricingRule">The pricing rule to set.</param>
+    public static void SetPricingRule(this Product product, PricingRule pricingRule)
+    {
+        product.PricingRule = pricingRule;
+    }
+}
