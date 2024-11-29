2d5794fb7ddc2ccd06793c4e1ae9476220d0340a
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-29 02:37:27 +0000

    Started implementing calculation logic

---
+++ Bright.Kata/Extensions/BasketItemExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Bright.Kata.Model;
+
+namespace Bright.Checkout.Extensions;
+
+/// <summary>
+/// Provides extension methods for the BasketItem class.
+/// </summary>
+public static class BasketItemExtensions
+{
+    /// <summary>
+    /// Sets the pricing rule for a product.
+    /// </summary>
+    /// <param name="item">The BasketItem to calculate total for.</param>
+    public static int GetTotalPrice(this BasketItem item)
+    {
+    
+        if (item.Product.PricingRule != null)
+        {
+            // Integer division will round down to the nearest whole number - this will give the number of valid multi-buy offers
+            int multiBuyQuantityInBasket = item.Product.PricingRule.MultiBuyQuantity / item.Quantity;
+            int multiBuyPrice = multiBuyQuantityInBasket * item.Product.PricingRule.MultiBuyPrice;
+        }
+        return item.Quantity * item.Product.StandardUnitPrice;
+    }
+}
