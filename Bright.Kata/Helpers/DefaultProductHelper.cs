feb2dc57f297cd96bb1f59f0640042200cf5d1b4
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-29 00:12:51 +0000

    Added XML comments and switch to using collection initialiser for products dictionary

--- Bright.Kata/Helpers/DefaultProductHelper.cs
+++ Bright.Kata/Helpers/DefaultProductHelper.cs
@@ -2,45 +2,54 @@
 
 namespace Bright.Checkout.Helpers;
 
+/// <summary>
+/// Provides helper methods for default product data.
+/// </summary>
 public static class DefaultProductHelper
 {
+    /// <summary>
+    /// Gets a dictionary of default products with their pricing rules.
+    /// </summary>
+    /// <returns>A dictionary of default products, keyed by product code.</returns>
     public static Dictionary<string, Product> GetDefaultProducts()
     {
         // Array of default product codes to avoid use of strings directly in the code.
         string[] defaultProductCodes = ["A", "B", "C", "D"];
 
-        Dictionary<string, Product> defaultProducts = new();
-
-        defaultProducts.Add(defaultProductCodes[0], new Product
+        Dictionary<string, Product> defaultProducts = new()
         {
-            ProductCode = defaultProductCodes[0], StandardUnitPrice = 50,
-            PricingRule = new PricingRule
             {
-                MultiBuyQuantity = 3,
-                MultiBuyPrice = 130
-            }
-        });
-
-        defaultProducts.Add(defaultProductCodes[1], new Product
-        {
-            ProductCode = defaultProductCodes[1], StandardUnitPrice = 30,
-            PricingRule = new PricingRule
+                defaultProductCodes[0], new Product
+                {
+                    ProductCode = defaultProductCodes[0], StandardUnitPrice = 50,
+                    PricingRule = new PricingRule
+                    {
+                        MultiBuyQuantity = 3,
+                        MultiBuyPrice = 130
+                    }
+                }
+            },
             {
-                MultiBuyQuantity = 2,
-                MultiBuyPrice = 45
-            }
-        });
+                defaultProductCodes[1], new Product
+                {
+                    ProductCode = defaultProductCodes[1], StandardUnitPrice = 30,
+                    PricingRule = new PricingRule
+                    {
+                        MultiBuyQuantity = 2,
+                        MultiBuyPrice = 45
+                    }
+                }
+            },
+            { defaultProductCodes[2], new Product
+            {
+                ProductCode = defaultProductCodes[2], StandardUnitPrice = 20
+            } },
+            { defaultProductCodes[3], new Product
+            {
+                ProductCode = defaultProductCodes[3], StandardUnitPrice = 15
+            } }
+        };
 
-        defaultProducts.Add(defaultProductCodes[2], new Product
-        {
-            ProductCode = defaultProductCodes[2], StandardUnitPrice = 20
-        });
-        
-        defaultProducts.Add(defaultProductCodes[3], new Product
-        {
-            ProductCode = defaultProductCodes[3], StandardUnitPrice = 15
-        });
-        
         return defaultProducts;
     }
 }
