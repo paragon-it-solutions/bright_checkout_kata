11bff3bd3b4a6f0090e145a40f538f019c9eaba7
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-29 03:57:04 +0000

    Updated interface with new methods

--- Bright.Checkout/Contracts/Data/IProductRepository.cs
+++ Bright.Checkout/Contracts/Data/IProductRepository.cs
@@ -5,4 +5,6 @@ namespace Bright.Checkout.Contracts.Data;
 public interface IProductRepository
 {
     public Dictionary<string, Product> GetProducts();
+    public bool SetProducts(Dictionary<string, Product> products);
+    public bool UpdateProductPricingRule(string productCode, PricingRule pricingRule);
 }
