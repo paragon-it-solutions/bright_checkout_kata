bf59668f3f25f191012292e4cac49a5af6fbbab9
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-29 01:07:40 +0000

    Added comments for pseudocode on program.cs for application flow

--- Bright.Kata/Program.cs
+++ Bright.Kata/Program.cs
@@ -1,3 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+// "Welcome to checkout kata!"
+
+// The program aims to simulate a simple checkout system that calculates the total
+// cost based on optional multi-buy pricing rules
+
+// "Would you like to provide custom products? (Y/N):"
+
+// If yes, Loop starts to allow user to create product and add to dictionary:
+    // User must enter ProductCode and StandardUnitPrice separated by a space, with optional PricingRules e.g. A 50 2 130
+    // User asked if they wish to add more products - can add as many products as they want
+
+// If NO
+    // The program will use default products
+    // "Would you like to customise default products? (Y/N):"
+
+// If yes, Loop starts to allow user to modify pricing of products in the dictionary
+    // User must enter ProductCode and new PricingRules separated by a space, e.g. A 50 2 130
+    // User asked if they wish to modify more products - can modify as many products as they want
+
+
+
+
+
