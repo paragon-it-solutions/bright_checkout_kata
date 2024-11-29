d14940b24bca59df35a44def93cc404e6299ae34
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-29 07:19:38 +0000

    Implemented ability for user to add custom products / update custom price rules

--- Bright.Checkout/Program.cs
+++ Bright.Checkout/Program.cs
@@ -1,50 +1,142 @@
 ﻿using Bright.Checkout.Contracts.Data;
 using Bright.Checkout.Contracts.Services;
+using Bright.Checkout.Data;
 using Bright.Checkout.Extensions;
+using Bright.Checkout.Helpers;
+using Bright.Checkout.Model;
 using Bright.Checkout.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
 
-// Register IProductRepository
-services.AddSingleton<IProductRepository>();
+// Register Repositories
+services.AddSingleton<IProductRepository, ProductRepository>();
 
-// Register ICheckoutService
+// Register Services
 services.AddTransient<ICheckoutService, CheckoutService>();
+services.AddTransient<IProductService, ProductService>();
 
 // Build the service provider
 ServiceProvider serviceProvider = services.BuildServiceProvider();
 
-// Verifies implementation of ICheckoutService exists
+// Gets the required services for use in Program
 var checkoutService = serviceProvider.GetRequiredService<ICheckoutService>();
+var productService = serviceProvider.GetRequiredService<IProductService>();
 
 Console.WriteLine("### Welcome to Bright Software Group - Checkout Kata! ###");
 
 Console.WriteLine(
-    "The program aims to simulate a simple checkout system that calculates the total cost based on optional multi-buy pricing rules\n");
+    "The program aims to simulate a simple checkout system that calculates the total cost based on optional multi-buy pricing rules\n"
+);
 
+
+//? Allows user to select customer products 
 string useCustomProducts;
-
 do
 {
-    Console.Write("Would you like to provide custom products? These will replace the default! (Y/N): ");
+    Console.Write(
+        "Would you like to provide custom products? These will replace the default! (Y/N): "
+    );
     useCustomProducts = ConsoleExtensions.GetCleanStringToUpper();
 } while (useCustomProducts != "Y" && useCustomProducts != "N");
 
 if (useCustomProducts == "Y")
 {
-    // Code for handling custom products
+    var customProducts = new Dictionary<string, Product>();
+
+    if (useCustomProducts == "Y")
+    {
+        string addMore;
+
+        do
+        {
+            Console.WriteLine(
+                "Enter product details in the format: ProductCode StandardUnitPrice [MultiBuyQuantity MultiBuyPrice]"
+            );
+            Console.WriteLine("Note items in brackets are optional.");
+            Console.WriteLine("Example: 'A 50 2 130' or 'A 50' are both valid inputs");
+            string input = ConsoleExtensions.GetCleanStringToUpper() ?? string.Empty;
+
+            Product? product = ProductHelper.GenerateProductFromString(input);
+
+            if (product != null)
+            {
+                customProducts.Add(product.ProductCode, product);
+            }
+            else
+            {
+                Console.WriteLine("Failed to add product.");
+            }
+
+            do
+            {
+                Console.Write("Do you want to add another product? (Y/N): ");
+                addMore = ConsoleExtensions.GetCleanStringToUpper();
+            } while (addMore != "Y" && addMore != "N");
+        } while (addMore == "Y");
+    }
+
+    // Sets the custom products to the productService if any exist
+    if (customProducts.Count > 0)
+    {
+        productService.SetProducts(customProducts);
+    }
 }
-// Code for using default products
-// Rest of your program...
 
-// If YES, Loop starts to allow user to create product and add to dictionary:
-// User must enter ProductCode and StandardUnitPrice separated by a space, with optional PricingRules e.g. A 50 2 130
-// User asked if they wish to add more products - can add as many products as they want
+// Gets collection of products
+Dictionary<string,Product> currentProducts = productService.GetProducts();
 
-// If NO
-// The program will use default products
-// "Would you like to customise default products? (Y/N):"
+//? Allows user to customise default product pricing rules
+string customiseDefaultProducts;
+do
+{
+    Console.Write(
+        "Would you customise the default product pricing rules? (Y/N): "
+    );
+    customiseDefaultProducts = ConsoleExtensions.GetCleanStringToUpper();
+} while (customiseDefaultProducts != "Y" && customiseDefaultProducts != "N");
+
+if (customiseDefaultProducts == "Y")
+{
+    if (customiseDefaultProducts == "Y")
+    {
+        string modifyMore;
+
+        do
+        {
+            Console.WriteLine(
+                "Enter SKU for product to update:"
+            );
+            string productCode = ConsoleExtensions.GetCleanStringToUpper();
+            
+            currentProducts.TryGetValue(productCode, out Product? productToUpdate);
+
+            if (productToUpdate != null)
+            {
+                Console.WriteLine("Enter Multi-Buy pricing rule in the format: MinimumQuantity MultiBuyPrice");
+                Console.WriteLine("EXAMPLE: 2 30");
+                
+                string multiBuyRules = ConsoleExtensions.GetCleanStringToUpper();
+                PricingRule? pricingRule = PricingRuleHelper.PricingRuleFromString(multiBuyRules);
+
+                if (pricingRule != null)
+                {
+                    currentProducts[productCode].SetPricingRule(pricingRule);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Product with SKU '{productCode}' not found.");
+            }
+            do
+            {
+                Console.Write("Do you want to modify another pricing rule? (Y/N): ");
+                modifyMore = ConsoleExtensions.GetCleanStringToUpper();
+            } while (modifyMore != "Y" && modifyMore != "N");
+        } while (modifyMore == "Y");
+    }
+}
+
 
 // If YES, Loop starts to allow user to modify pricing of products in the dictionary
 // User must enter ProductCode and new PricingRules separated by a space, e.g. A 50 2 130
