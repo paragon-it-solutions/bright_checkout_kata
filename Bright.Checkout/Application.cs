60347d48d8498b90162dc4b7a6a814bb31b5b05d
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-29 08:47:11 +0000

    Moved all console logic to Application.cs calling appropraite handlers with DI injected as needed

---
+++ Bright.Checkout/Application.cs
@@ -0,0 +1,21 @@
+﻿using Bright.Checkout.Contracts.Core;
+using Bright.Checkout.Contracts.Services;
+
+namespace Bright.Checkout
+{
+    public class Application(
+        ICustomProductHandler customProductHandler,
+        IPricingRuleHandler pricingRuleHandler,
+        ICheckoutHandler checkoutHandler)
+    {
+        public void Run()
+        {
+            Console.WriteLine("### Welcome to Bright Software Group - Checkout Kata! ###");
+            Console.WriteLine("The program aims to simulate a simple checkout system that calculates the total cost based on optional multi-buy pricing rules\n");
+
+            customProductHandler.HandleCustomProducts();
+            pricingRuleHandler.HandleCustomPricingRules();
+            checkoutHandler.ProcessCheckout();
+        }
+    }
+}
