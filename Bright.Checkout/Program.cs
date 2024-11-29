e6ba25d5dca0a99a21ff6fd77fe2d021ecefb894
Stephen Gibson <stephen_gibson@paragonitsolutions.co.uk>
2024-11-29 03:44:33 +0000

    Added DI package and began to set up DI for IProductRepository and ICheckoutService

--- Bright.Checkout/Program.cs
+++ Bright.Checkout/Program.cs
@@ -1,4 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.DependencyInjection;
+using Bright.Checkout.Contracts.Data;
+using Bright.Checkout.Contracts.Services;
+using Bright.Checkout.Model;
+using Bright.Checkout.Helpers;
+using Bright.Checkout.Services;
+
+var services = new ServiceCollection();
+
+// Create the dictionary of products
+Dictionary<string, Product> products = DefaultProductHelper.GetDefaultProducts();
+
+// Register IProductRepository
+services.AddSingleton<IProductRepository>();
+
+// Register ICheckoutService
+services.AddTransient<ICheckoutService, CheckoutService>();
+
+// Build the service provider
+var serviceProvider = services.BuildServiceProvider();
+
+// Verifies implementation of ICheckoutService exists
+var checkoutService = serviceProvider.GetRequiredService<ICheckoutService>();
 
 Console.WriteLine("Hello, World!");
 
@@ -9,7 +31,7 @@ Console.WriteLine("Hello, World!");
 
 // "Would you like to provide custom products? (Y/N):"
 
-// If yes, Loop starts to allow user to create product and add to dictionary:
+// If YES, Loop starts to allow user to create product and add to dictionary:
     // User must enter ProductCode and StandardUnitPrice separated by a space, with optional PricingRules e.g. A 50 2 130
     // User asked if they wish to add more products - can add as many products as they want
 
@@ -17,11 +39,16 @@ Console.WriteLine("Hello, World!");
     // The program will use default products
     // "Would you like to customise default products? (Y/N):"
 
-// If yes, Loop starts to allow user to modify pricing of products in the dictionary
+// If YES, Loop starts to allow user to modify pricing of products in the dictionary
     // User must enter ProductCode and new PricingRules separated by a space, e.g. A 50 2 130
     // User asked if they wish to modify more products - can modify as many products as they want
 
+// IF NO
+    // The program will proceed to the checkout
 
+// Initialise CheckoutService with products
+
+// "Please scan items to add to your basket:"
 
 
 
